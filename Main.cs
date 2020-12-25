4759cc56f7648a09d899c09d95c2da1a2c5973bd
imkyran <30765677+imkyran@users.noreply.github.com>
2020-12-24 19:59:18 -0800

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,46 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace TestMod
+{
+    public static class BuildInfo
+    {
+        public const string Name = "HeadFlipper";
+        public const string Author = "Kyran";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class TestMod : MelonMod
+    {
+        public static bool HeadFlipper = false;
+        public static bool UIManagerInit = false;
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("HeadFlipper v" + BuildInfo.Version + " Loaded.");
+        }
+
+        public override void OnUpdate()
+        {
+            bool ToggleHeadFlipper = Input.GetKeyDown(KeyCode.Tab);
+            if (ToggleHeadFlipper)
+            {
+                HeadFlipper = !HeadFlipper;
+                if (HeadFlipper && UIManagerInit == true)
+                {
+                    VRCVrCamera.field_Private_Static_VRCVrCamera_0.GetComponentInChildren<NeckMouseRotator>().rotationRange = new Vector3(float.MinValue, 0f, float.MaxValue);
+                }
+                else if (!HeadFlipper && UIManagerInit == true)
+                {
+                    VRCVrCamera.field_Private_Static_VRCVrCamera_0.GetComponentInChildren<NeckMouseRotator>().rotationRange = new Vector3(0f, 0f, 0f);
+                }
+            }
+        }
+
+        public override void VRChat_OnUiManagerInit() // Runs upon VRChat's UiManager Initialization. Only runs if the Mod is used in VRChat.
+        {
+            UIManagerInit = true;
+        }
+    }
+}
